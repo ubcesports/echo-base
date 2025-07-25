9bdeb524a66883b45145b7d0b852941f204f0466
joshhwuu <joshuawu2004@gmail.com>
2025-07-24 21:01:42 -0700

    go clean

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,10 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
-
 	"github.com/ubcesports/echo-base/config"
-	"github.com/ubcesports/echo-base/internal/database/db"
 )
 
 func main() {
 	config.LoadEnv(".env")
-	db.Init()
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/")
 
 }
