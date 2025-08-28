17ec8f9e26068d12df54ae832504b10a37c1da09
Brian Shao <brian@jzshao.com>
2025-08-28 13:26:27 -0700

    feat: docker setup

--- internal/database/db.go
+++ internal/database/db.go
@@ -12,18 +12,8 @@ import (
 var DB *sql.DB
 
 func Init() {
-	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s search_path=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_SCHEMA"),
-	)
-
 	var err error
-	DB, err = sql.Open("postgres", dsn)
+	DB, err = sql.Open("postgres", os.Getenv("EB_DSN"))
 	if err != nil {
 		log.Fatal("DB open error:", err)
 	}
