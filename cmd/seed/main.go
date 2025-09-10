001349e8c3f92a3786407fa3858bbe55b23e63a4
Brian Shao <brian@jzshao.com>
2025-09-10 13:28:01 -0700

    feat: database seed cli (#18)

---
+++ cmd/seed/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+
+	"github.com/ubcesports/echo-base/config"
+	"github.com/ubcesports/echo-base/internal/database"
+	"github.com/ubcesports/echo-base/internal/handlers"
+)
+
+func main() {
+	config.LoadEnv(".env")
+
+	database.Init()
+	defer database.Close()
+
+	if len(os.Args) < 3 {
+		println("please specify operation and value")
+		os.Exit(1)
+	}
+
+	if os.Args[1] != "apikey" {
+		println("operation not supported")
+		os.Exit(1)
+	}
+
+	response, err := handlers.CreateApiKey(os.Args[2])
+	if err != nil {
+		println("error while generating api key:", err.Error())
+		os.Exit(1)
+	}
+
+	println("generated api key!")
+	println("key id:", response.KeyID)
+	println("token:", response.APIKey)
+}
