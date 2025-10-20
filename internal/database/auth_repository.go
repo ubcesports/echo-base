ca11fe55964021224330515bbe40d6d688090c15
Jaden Hums <jadenhums51@gmail.com>
2025-10-19 17:30:30 -0700

    add database auth methods and update handlers

--- internal/database/auth_repository.go
+++ internal/database/auth_repository.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	"database/sql"
+	"time"
 
 	"github.com/ubcesports/echo-base/internal/interfaces/auth"
 )
@@ -16,13 +17,37 @@ func NewAuthRepository(db *sql.DB) *AuthRepository {
 }
 
 func (r *AuthRepository) Store(ctx context.Context, app *auth.Application) error {
-	return nil
+	query := `
+        INSERT INTO application (app_name, key_id, hashed_key)
+        VALUES ($1, $2, $3, $4, $5)
+    `
+	_, err := r.db.ExecContext(ctx, query,
+		app.AppName, app.KeyId, app.HashedKey)
+	return err
 }
 
 func (r *AuthRepository) FindKeyById(ctx context.Context, keyId string) (*auth.Application, error) {
-	return nil, nil
+	query := `
+        SELECT app_name, key_id, hashed_key
+        FROM application 
+        WHERE key_id = $1
+    `
+
+	app := &auth.Application{}
+
+	err := r.db.QueryRowContext(ctx, query, keyId).Scan(
+		&app.KeyId, &app.AppName, &app.HashedKey,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return app, nil
 }
 
 func (r *AuthRepository) UpdateLastUsed(ctx context.Context, keyId string) error {
-	return nil
+	query := `UPDATE application SET last_used_at = $1 WHERE key_id = $2`
+	_, err := r.db.ExecContext(ctx, query, time.Now(), keyId)
+	return err
 }
